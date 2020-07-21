41c20d389c9ba0f6ff4e234e9bd2ecd3a10c1469
Christopher Nielsen <c.consult.christopher@gmail.com>
2020-07-21 19:31:05 +0200

    StartGameScreen

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "black",
+    shadowOffset: { height: 2, width: 0 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "white",
+    elevation: 4,
+    padding: 20,
+    borderRadius: 6,
+  },
+});
+
+export default Card;
